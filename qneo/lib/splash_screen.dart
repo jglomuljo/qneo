0f15bb1b982b7a1d5151363a12c2354a5a4674d3
jglomuljo <65459979+jglomuljo@users.noreply.github.com>
2021-07-22 18:06:42 +0800

    Added splash screen

---
+++ qneo/lib/splash_screen.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:qneo/homepage.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashscreenState createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => Home())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomRight,
+              colors: [Color(0xFF4CA9DF), Color(0xFF292E91)],
+            ),
+          ),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(children: [
+                  Image.asset('assets/images/logo.png',
+                      height: 300.0, width: 300.0),
+                  CircularProgressIndicator(),
+                ]),
+              ])),
+    );
+  }
+}
