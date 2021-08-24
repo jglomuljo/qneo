71bbcd0b770080c3cfa1a63cdfd6d1c7a3f7ee0e
jglomuljo <65459979+jglomuljo@users.noreply.github.com>
2021-08-24 13:21:34 +0800

    Edited at Locations Page

--- qneo/lib/Screens/5_locationspage.dart
+++ qneo/lib/Screens/5_locationspage.dart
@@ -32,7 +32,219 @@ class _LocationsPage extends State<LocationsPage> {
             ),
           ],
         ),
-        body: Center(),
+        body: Container(
+          child: Column(
+            children: [
+              Expanded(
+                child: ListView(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 10, top: 25),
+                      height: 200,
+                      padding: const EdgeInsets.only(
+                          left: 20, right: 20, bottom: 20),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: const BorderRadius.only(
+                            bottomLeft: Radius.circular(80.0),
+                            topLeft: Radius.circular(80.0),
+                          ),
+                          boxShadow: [
+                            new BoxShadow(
+                              color: Colors.blue.withOpacity(0.3),
+                              offset: new Offset(-10.0, 0.0),
+                              blurRadius: 20.0,
+                              spreadRadius: 4.0,
+                            ),
+                          ],
+                        ),
+                        padding: const EdgeInsets.only(
+                          left: 32,
+                          top: 50.0,
+                          bottom: 50,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "CHECKED OUT",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 12),
+                            ),
+                            const SizedBox(
+                              height: 2,
+                            ),
+                            Text(
+                              "F612",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 10, top: 25),
+                      height: 200,
+                      padding: const EdgeInsets.only(
+                          left: 20, right: 20, bottom: 20),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: const BorderRadius.only(
+                            bottomRight: Radius.circular(80.0),
+                            topRight: Radius.circular(80.0),
+                          ),
+                          boxShadow: [
+                            new BoxShadow(
+                              color: Colors.grey.withOpacity(0.3),
+                              offset: new Offset(-10.0, 0.0),
+                              blurRadius: 20.0,
+                              spreadRadius: 4.0,
+                            ),
+                          ],
+                        ),
+                        padding: const EdgeInsets.only(
+                          left: 32,
+                          top: 50.0,
+                          bottom: 50,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "CHECKED IN",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 12),
+                            ),
+                            const SizedBox(
+                              height: 2,
+                            ),
+                            Text(
+                              "F612",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 10, top: 25),
+                      height: 200,
+                      padding: const EdgeInsets.only(
+                          left: 20, right: 20, bottom: 20),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: const BorderRadius.only(
+                            bottomLeft: Radius.circular(80.0),
+                            topLeft: Radius.circular(80.0),
+                          ),
+                          boxShadow: [
+                            new BoxShadow(
+                              color: Colors.grey.withOpacity(0.3),
+                              offset: new Offset(-10.0, 0.0),
+                              blurRadius: 20.0,
+                              spreadRadius: 4.0,
+                            ),
+                          ],
+                        ),
+                        padding: const EdgeInsets.only(
+                          left: 32,
+                          top: 50.0,
+                          bottom: 50,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "CHECKED OUT",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 12),
+                            ),
+                            const SizedBox(
+                              height: 2,
+                            ),
+                            Text(
+                              "J304",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 10, top: 25),
+                      height: 200,
+                      padding: const EdgeInsets.only(
+                          left: 20, right: 20, bottom: 20),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: const BorderRadius.only(
+                            bottomRight: Radius.circular(80.0),
+                            topRight: Radius.circular(80.0),
+                          ),
+                          boxShadow: [
+                            new BoxShadow(
+                              color: Colors.grey.withOpacity(0.3),
+                              offset: new Offset(-10.0, 0.0),
+                              blurRadius: 20.0,
+                              spreadRadius: 4.0,
+                            ),
+                          ],
+                        ),
+                        padding: const EdgeInsets.only(
+                          left: 32,
+                          top: 50.0,
+                          bottom: 50,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "CHECKED IN",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 12),
+                            ),
+                            const SizedBox(
+                              height: 2,
+                            ),
+                            Text(
+                              "J304",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       );
 
   Widget buildDatePicker() => SizedBox(
@@ -72,4 +284,14 @@ class _LocationsPage extends State<LocationsPage> {
       ..removeCurrentSnackBar()
       ..showSnackBar(snackBar);
   }
+}
+
+class Places {
+  final String status;
+  final String room;
+  final String building;
+  final DateTime date;
+  final TimeOfDay time;
+
+  Places(this.status, this.room, this.building, this.date, this.time);
 }
