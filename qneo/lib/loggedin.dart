68574368875a4ab21058907bb17623144731589c
Edacity11 <edrickedrick1@gmail.com>
2021-09-27 23:32:50 +0800

    Changed LoggedIn.dart from stateless to stateful

--- qneo/lib/loggedin.dart
+++ qneo/lib/loggedin.dart
@@ -3,51 +3,29 @@ import 'package:flutter/material.dart';
 import 'package:qneo/Screens/1_loginpage.dart';
 import 'package:qneo/Screens/2_home.dart';
 
-// class LoggedIn extends StatefulWidget {
-//   const LoggedIn({Key? key}) : super(key: key);
-
-//   @override
-//   _LoggedInState createState() => _LoggedInState();
-// }
-
-// class _LoggedInState extends State<LoggedIn> {
-//   @override
-//   Widget build(BuildContext context) => Scaffold(
-//         body: StreamBuilder(
-//           stream: FirebaseAuth.instance.authStateChanges(),
-//           builder: (context, snapshot) {
-//             if (snapshot.connectionState == ConnectionState.waiting) {
-//               return Center(
-//                   child: CircularProgressIndicator(
-//                       backgroundColor: Colors.greenAccent,
-//                       valueColor:
-//                           new AlwaysStoppedAnimation<Color>(Colors.red)));
-//             } else if (snapshot.hasData) {
-//               return ProfilePage();
-//             } else if (snapshot.hasError) {
-//               return Center(child: Text('Something Went Wrong!'));
-//             } else {
-//               return Home();
-//             }
-//           },
-//         ),
-//       );
-// }
-
-class LoggedIn extends StatelessWidget {
+class LoggedIn extends StatefulWidget {
   const LoggedIn({Key? key}) : super(key: key);
 
+  @override
+  _LoggedInState createState() => _LoggedInState();
+}
+
+class _LoggedInState extends State<LoggedIn> {
   @override
   Widget build(BuildContext context) => Scaffold(
         body: StreamBuilder(
           stream: FirebaseAuth.instance.authStateChanges(),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
+              return Center(
+                  child: CircularProgressIndicator(
+                      backgroundColor: Colors.greenAccent,
+                      valueColor:
+                          new AlwaysStoppedAnimation<Color>(Colors.red)));
             } else if (snapshot.hasData) {
               return ProfilePage();
             } else if (snapshot.hasError) {
-              return Center(child: Text('Something went Wrong'));
+              return Center(child: Text('Something Went Wrong!'));
             } else {
               return Home();
             }
@@ -55,3 +33,25 @@ class LoggedIn extends StatelessWidget {
         ),
       );
 }
+
+// class LoggedIn extends StatelessWidget {
+//   const LoggedIn({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) => Scaffold(
+//         body: StreamBuilder(
+//           stream: FirebaseAuth.instance.authStateChanges(),
+//           builder: (context, snapshot) {
+//             if (snapshot.connectionState == ConnectionState.waiting) {
+//               return Center(child: CircularProgressIndicator());
+//             } else if (snapshot.hasData) {
+//               return ProfilePage();
+//             } else if (snapshot.hasError) {
+//               return Center(child: Text('Something went Wrong'));
+//             } else {
+//               return Home();
+//             }
+//           },
+//         ),
+//       );
+// }
